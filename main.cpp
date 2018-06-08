1cb548fca5f95ff320804fb75049fcf36a8cf6aa
demi <meintomatenfrosch@gmail.com>
2018-06-08 13:03:27 -0400

    creating an object and giving texture

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@ int main()
 {
     
     sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize) ;
+    sf::RectangleShape player(sf::Vector2f(100, 100)) ; //sets size of object
+    player.setFillColor(sf::Color::Red);
+    player.setOrigin(50.0f, 50.0f); //changes origin of object to the center
+    
+    //creating textures
+    sf::Texture playerTexture ;
+    playerTexture.loadFromFile("mushroom.png") ;
+    player.setTexture(&playerTexture) ;
     
     while(window.isOpen())
     {
@@ -26,14 +34,49 @@ int main()
                     
                     printf("New Window Width: %i New window height: %i \n",evnt.size.width, evnt.size.height) ;
                     break ;
-                case sf::Event::TextEntered:
+                /*case sf::Event::TextEntered: //prints out whatever text the user inputs while running the program
                     if(evnt.text.unicode < 128)
                     {
                         printf("%c", evnt.text.unicode) ;
-                    break ;
-                    
-                    
+                    }
+                    break ; */                   
             }
         }
+        
+        //button controls
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+        {
+            player.move(-0.1f, 0.0f);
+        }
+        
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+        {
+            player.move(0.1f, 0.0f);
+        }
+        
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+        {
+            player.move(0.0f, -0.1f);
+        }
+        
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+        {
+            player.move(0.0f, 0.1f);
+        }
+        
+        //mouse controls
+//         if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+//         {
+//             sf::Vector2i mousePos = sf::Mouse::getPosition(window) ;
+//             player.setPosition((float)mousePos.x, (float)mousePos.y) ;
+//         }
+
+
+        
+        window.clear(); //can also set a bg color with sf::Color::[color]
+        window.draw(player) ; //will leave the drawn path in the window unless window.clear is called 
+        window.display() ;
     }
+    
+
 }
