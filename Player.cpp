f2ab78558c2a45d7da14c9de003cb9b161be3ef0
demi <meintomatenfrosch@gmail.com>
2018-06-11 11:47:25 -0400

    fixed Player class and changed direction of texture

--- Player.cpp
+++ Player.cpp
@@ -1,12 +1,61 @@
 #include "Player.h"
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <string>
-#include <iostream>
 #include "Animation.h"
 #include "Animation.cpp"
 
 
 using namespace std ;
+using namespace sf ;
 
 
+Player::Player(Texture* texture, Vector2u imageCount, float switchTime, float speed) : changedir(texture, imageCount, switchTime)
+{
+    this->speed = speed ;
+    row = 0 ;
+    faceRight = true ;
+    
+    body.setSize(Vector2f(150, 100)) ;
+    body.setPosition(206.0f, 206.0f);
+    body.setTexture(texture) ;
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::Update(float deltaTime) 
+{
+    Vector2f movement(0.0f, 0.0f) ;
+    
+    if(Keyboard::isKeyPressed(Keyboard::Left))
+        movement.x -= speed * deltaTime ;
+    if(Keyboard::isKeyPressed(Keyboard::Right))
+        movement.x += speed * deltaTime ;
+    
+    if(movement.x == 0.0f)
+    {
+        row = 0 ;
+    }
+    else
+    {
+        row = 1 ;
+        if(movement.x > 0.0f)
+        {
+            faceRight = true ;
+        }
+        else
+        {
+            faceRight = false ;
+        }
+    }
+    
+    changedir.Update(row, deltaTime, faceRight);
+    body.setTextureRect(changedir.uvRect);
+    body.move(movement) ;
+
+}
+
+void Player::Draw(RenderWindow& window)
+{
+    window.draw(body);
+}
--- Player.h
+++ Player.h
@@ -1,22 +1,31 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <string>
-#include <iostream>
 #include "Animation.h"
-#include "Animation.cpp"
+
+
 using namespace std ;
+using namespace sf ;
+
 
 class Player
 {
 public:
-    Player() ;
-private:
-    sf::RectangleShape body ;
-    Animation animation ;
-    unsigned int row ; //unsigned ensures that integer is never less than 0
+    //Default constructor
+    Player(Texture* texture, Vector2u imageCount, float switchTime, float speed);
     
+    //Destructor
+    ~Player();
+    
+    void Update(float deltaTime) ;
+    void Draw(RenderWindow& window) ;
+    
+private:
+    RectangleShape body ;
+    Animation changedir ;
+    unsigned int row ; //unsigned means it will not be less than zero
+    float speed ;
+    bool faceRight ;
+
 };
 
 #endif // PLAYER_H
