35e4c201dee883593f624796336186c71b01a4d5
demi <meintomatenfrosch@gmail.com>
2018-06-11 12:14:47 -0400

    uncluttered code

--- main.cpp
+++ main.cpp
@@ -13,28 +13,17 @@ int main()
 {
     
     RenderWindow window(VideoMode(512, 512), "SFML Tutorial", Style::Close | Style::Titlebar | Style::Resize) ;
-//   RectangleShape player(Vector2f(150, 100)) ; //sets size of object
-//     player.setFillColor(Color::Red);
-//    player.setOrigin(50.0f, 50.0f); //changes origin of object to the center
-//    player.setPosition(206.0f, 206.0f);
     
     //creating textures
     Texture playerTexture ;
     playerTexture.loadFromFile("../Textures/tux_from_linux.png") ; // UV coordinates
-//     Texture mushroom ;
-//     mushroom.loadFromFile("../Textures/mushroom.png") ;
-//    player.setTexture(&playerTexture) ;
+
     
     Player player(&playerTexture, Vector2u(3, 9), 0.3f, 100.0f) ;
     
     float deltaTime = 0.0f ;
     Clock clock ;
     
-    Vector2u textureSize = playerTexture.getSize() ;
-    textureSize.x /= 3 ;
-    textureSize.y /= 9 ;
-    
-//    player.setTextureRect(IntRect(textureSize.x * 2, textureSize.y * 8, textureSize.x, textureSize.y)) ;
     
     while(window.isOpen())
     {
@@ -48,17 +37,6 @@ int main()
                 case Event::Closed:
                     window.close();
                     break ;
-                case Event::Resized:
-                    //cout << "New window width: " << evnt.size.width << " New window height: " << evnt.size.height << endl ;
-                    
-                    printf("New Window Width: %i New window height: %i \n",evnt.size.width, evnt.size.height) ;
-                    break ;
-                /*case Event::TextEntered: //prints out whatever text the user inputs while running the program
-                    if(evnt.text.unicode < 128)
-                    {
-                        printf("%c", evnt.text.unicode) ;
-                    }
-                    break ; */                   
             }
             
         }
